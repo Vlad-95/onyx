4dca0d544426c5140f0aaf4a459e2c61bc4e2252
vlad_z <vzv@siluetstudio.com>
2022-11-02 16:48:22 +0300

    certificates page

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import 'slick-carousel';
+import { Fancybox } from '@fancyapps/ui/src/Fancybox/Fancybox.js';
 
 import intro from './modules/intro';
 import menu from './modules/menu';
